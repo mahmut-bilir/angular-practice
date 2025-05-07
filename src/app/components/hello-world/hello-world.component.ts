5bde71618b69c2c694757058fa83388aaf039980
Mahmut Bilir <mahmut-bilir@hotmail.com>
2025-05-07 18:55:15 +0300

    add HelloWorldComponent with template, styles, and tests

---
+++ src/app/components/hello-world/hello-world.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-world',
+  standalone: true,
+  imports: [],
+  templateUrl: './hello-world.component.html',
+  styleUrls: ['./hello-world.component.css']
+})
+export class HelloWorldComponent {
+
+}
