5b6e5f1595dbc0cc201af015e5ff4d3053ab727a
Mahmut Bilir <mahmut-bilir@hotmail.com>
2025-05-07 18:55:58 +0300

    add HelloWorldComponent to AppComponent imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HelloWorldComponent } from './components/hello-world/hello-world.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HelloWorldComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular-practice';
