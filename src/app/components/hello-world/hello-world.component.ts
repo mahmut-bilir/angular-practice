7b3e234b159e6c79f363fe3b058a28536bf08d40
Mahmut Bilir <mahmut-bilir@hotmail.com>
2025-05-07 23:06:44 +0300

    *ngIf example

--- src/app/components/hello-world/hello-world.component.ts
+++ src/app/components/hello-world/hello-world.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hello-world',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './hello-world.component.html',
   styleUrls: ['./hello-world.component.css']
 })
@@ -12,5 +13,6 @@ export class HelloWorldComponent {
   imageUrl = "https://example.com/image.jpg";
   sayHi() {
     alert("Hello!");
-  }
+  };
+  isLoggedIn = false;
 }
