251e816dfaeb278ff1811656c47d963edbb8e354
Mahmut Bilir <mahmut-bilir@hotmail.com>
2025-05-19 00:32:36 +0300

    add routing configuration for Home and About components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
 
-export const routes: Routes = [];
+
+
+export const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent }
+
+];
