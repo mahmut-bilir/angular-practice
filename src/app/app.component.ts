08cf6ef08700326bb4c3257e273267a360cc4582
Mahmut Bilir <mahmut-bilir@hotmail.com>
2025-05-21 00:17:53 +0300

    fix: implement OnInit lifecycle and fetch posts in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { HelloWorldComponent } from './components/hello-world/hello-world.component';
 import { UserCardComponent } from './components/user-card/user-card.component';
 import { LoggerService } from './services/logger.service'
-import { FormsModule, FormControl, ReactiveFormsModule } from '@angular/forms'
-
+import { FormsModule, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { PostService } from './services/post.service';
+import { CommonModule } from '@angular/common'
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, RouterLink, HelloWorldComponent,
-    UserCardComponent, FormsModule, ReactiveFormsModule],
+    UserCardComponent, FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  posts: any[] = [];
 
-  constructor(private logger: LoggerService) { }
+
+  constructor(private logger: LoggerService, private PostService: PostService) { }
+  ngOnInit() {
+    this.PostService.getPosts().subscribe((data) => {
+      this.posts = data
+    });
+  }
+
+
   logMessage() {
     this.logger.log("Hello Mahmut Bilir!");
   }
